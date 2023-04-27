bc4b4684751ad3693399465fd606be03d3225236
akie123 <aalhadakate@gmail.com>
2023-04-28 00:03:53 +0530

    update

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
-const sockets = io('http://localhost:3005', { autoConnect: true, forceNew: true });
+const sockets = io('https://eclinic-connect.onrender.com', { autoConnect: true, forceNew: true });
 
 export default sockets;
