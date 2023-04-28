1319fb989cdc557e7b3ac4ddc7dd1148a159d421
akie123 <aalhadakate@gmail.com>
2023-04-28 08:00:41 +0530

    update

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -47,16 +47,7 @@ const Main = (props) => {
   return (
     <MainContainer>
 
-      <Row>
-        <Label htmlFor="roomName">Room Name</Label>
-        <Input type="text" id="roomName" ref={roomRef} />
-      </Row>
-      <Row>
-        <Label htmlFor="userName">User Name</Label>
-        <Input type="text" id="userName" ref={userRef} />
-      </Row>
-      <JoinButton onClick={clickJoin}> Join </JoinButton>
-      {err ? <Error>{errMsg}</Error> : null}
+
     </MainContainer>
   );
 };
